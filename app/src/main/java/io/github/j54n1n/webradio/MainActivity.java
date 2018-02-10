e324a08676c29f545229d944b9ca79f548f4c3bf
Julian Sanin <j54n1n+noreply@gmail.com>
2018-02-10 12:16:02 +0100

    Initial commit

---
+++ app/src/main/java/io/github/j54n1n/webradio/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.j54n1n.webradio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
